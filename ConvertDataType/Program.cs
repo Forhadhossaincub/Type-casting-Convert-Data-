0f067cbd4ab9d7dbe8699cab97bc97e1da77dc80
NET <NET@DESKTOP-NSMUMFG>
2022-10-19 12:35:36 +0600

    Add project files.

---
+++ ConvertDataType/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertDataType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age;
+            decimal yearlyIncome;
+
+            Console.Write("Your Name : ");
+            name = Console.ReadLine();
+
+            Console.Write("Your Age : ");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Your Yearly Income : ");
+            yearlyIncome = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine($"{name} of age {age} income/year {yearlyIncome:0.00}");
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
